0857ea927ba6a3c64ed5d65a5934c03b88e8445c
Hank <hank.shen@itrue.com.tw>
2016-11-02 01:43:08 +0800

    fix nav bugs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
 
   def nav_link(link_text, link_path)
-    from = Rails.application.routes.recognize_path(request.url)
-    path = Rails.application.routes.recognize_path(link_path)
+    from_control = Rails.application.routes.recognize_path(request.url)[:controller].split('/')[0]
+    path_control = Rails.application.routes.recognize_path(link_path)[:controller].split('/')[0]
 
-    class_name = %w(nav-item) << (from[:controller] == path[:controller] ? 'active' : '')
+    class_name = %w(nav-item) << (from_control == path_control ? 'active' : '')
     content_tag :li, class: class_name.join(' ') do
       link_to link_text, link_path, class: 'nav-link'
     end
