74815585ba787c9a02ea49f7daf402b61071b65b
Hank <hank.shen@itrue.com.tw>
2016-11-02 01:31:39 +0800

    fix nav active label

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def nav_link(link_text, link_path)
+    from = Rails.application.routes.recognize_path(request.url)
+    path = Rails.application.routes.recognize_path(link_path)
+
+    class_name = %w(nav-item) << (from[:controller] == path[:controller] ? 'active' : '')
+    content_tag :li, class: class_name.join(' ') do
+      link_to link_text, link_path, class: 'nav-link'
+    end
+  end
+
 end
