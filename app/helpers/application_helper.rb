bd690ff19c95586782baca1c72ac22e23ddce52e
Hank <hank.shen@itrue.com.tw>
2016-11-11 14:34:16 +0800

    fix mobile nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,15 @@
 module ApplicationHelper
 
-  def nav_link(link_text, link_path)
+  def nav_link(link_text, link_path, options = nil)
     from_control = Rails.application.routes.recognize_path(request.url)[:controller].split('/')[0]
     path_control = Rails.application.routes.recognize_path(link_path)[:controller].split('/')[0]
 
-    class_name = %w(nav-item) << (from_control == path_control ? 'active' : '')
-    content_tag :li, class: class_name.join(' ') do
-      link_to link_text, link_path, class: 'nav-link'
+    klass = %w(nav-item) << (from_control == path_control ? 'active' : '')
+    icon  = content_tag :i, '', class: options[:icon], 'aria-hidden' => true if options.try(:[], :icon)
+    content_tag :li, class: klass.join(' ') do
+      link_to link_path, class: 'nav-link' do
+        link_text = icon ? "#{icon}#{link_text}".html_safe : link_text
+      end
     end
   end
 
